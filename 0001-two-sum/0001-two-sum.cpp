7fa854e7934abffb7faf36f058adcb19c391618a
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-07-01 20:49:28 +0530

    Time: 35 ms (34.94%), Space: 14.1 MB (81.6%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n=arr.size();
+        
+      for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+     
+        if(arr[i]+arr[j]==target)
+      return {i,j}; 
+        }
+        }
+        
+    
+
+     return {-1,-1};
+     
+    }
+};
