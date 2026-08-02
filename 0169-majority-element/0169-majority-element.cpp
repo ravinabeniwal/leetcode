ee098bf1ac4967b4b4bc46d1dca72556abcd46a9
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-08-02 16:28:36 +0530

    Time: 0 ms (100%), Space: 28.2 MB (39.55%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]) count++;
+            else 
+            count=1;
+        
+        if(count>n/2) return nums[i];
+        }
+        return nums[0];
+    }
+};
