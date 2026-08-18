5563a63e7fcaf6310a44ced7667dbc13af071736
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-08-18 21:53:36 +0530

    Time: 25 ms (23.55%), Space: 37 MB (5%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<int>st;
+        for(char ch:s){
+            if(ch=='*')
+            st.pop();
+            else
+            st.push(ch);
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
