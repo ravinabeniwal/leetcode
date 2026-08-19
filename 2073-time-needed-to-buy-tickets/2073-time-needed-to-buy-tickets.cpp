a00a1a7836e3266e50345d1f4b064ec685e5274f
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-08-19 22:55:51 +0530

    Time: 0 ms (100%), Space: 11.1 MB (42.78%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<int>q;
         int n=tickets.size();
+        queue<int>q;
         for(int i=0;i<n;i++){
             q.push(i);
         }
-        int time=0;
-        while(tickets[k]!=0){
-            tickets[q.front()]--;
-            if(tickets[q.front()])
-            q.push(q.front());
+        int count=0, i=0;
+        while(!q.empty()){
+            int person=q.front();
             q.pop();
-            time++;
+            tickets[person]--;
+            count++;
+            if(k==person && tickets[person]==0){
+                break;
+                
+            }
+
+        if(tickets[person]>0){
+                q.push(person);
+                
+            }
         }
-    return time;
+        return count;
     }
 };
