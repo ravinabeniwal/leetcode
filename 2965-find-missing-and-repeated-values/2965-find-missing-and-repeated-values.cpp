1b556c2b2306de72527d7211e925882070dde4ca
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-08-24 21:33:12 +0530

    Time: 72 ms (5.17%), Space: 27 MB (57.39%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int>arr;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++)
-            arr.push_back(grid[i][j]);
+        vector<int> arr;
+        int row = grid.size();
+        int col = grid[0].size();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr.push_back(grid[i][j]);
+            }
         }
-         int n = arr.size();
         sort(arr.begin(), arr.end());
 
-        vector<int> ans;
-        int r=0;
-        for(int i = 1; i < n; i++) {
-            if(arr[i] == arr[i-1]) {
-                r+=arr[i];
-                ans.push_back(arr[i]);
+        int n = arr.size();
+        int repeated = -1;
+        int missing = -1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                repeated = arr[i];
                 break;
             }
         }
-
-    long long sum1=0;
-    long long  sum2=0;
-        for(int i = 0; i <n; i++) {
-           sum1+=arr[i];}
-            for(int i = 1; i <= n; i++) {
-           sum2+=i;}
-           int diff=sum2-(sum1-r);
-           ans.push_back(diff);
-            
-        
-
-        return ans;
+      for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == i) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                missing = i;
+                break;
+            }
+        }
+        return {repeated, missing};
     }
 };
