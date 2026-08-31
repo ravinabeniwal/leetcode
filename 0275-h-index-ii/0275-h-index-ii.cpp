7ec2b1b171aa9b1bb77868a5e1737d219acaabd2
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-08-31 21:20:04 +0530

    Time: 0 ms (100%), Space: 22.4 MB (78.11%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(citations[i]>=n-i){
+                count=n-i;
+                break;
+            }
+        }
+        return count;
+    }
+};
