3594f0a43764832028d587f63e3f0ce8b1221f92
Ravina Beniwal <ravinabeniwal390@gmail.com>
2026-09-04 10:36:42 +0530

    Time: 2728 ms (5.04%), Space: 332.8 MB (5.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       int left=0,ans=0;
-       for(int right=0;right<s.size();right++){
-        for(int i=left;i<right;i++){
-            if(s[i]==s[right]){
-                left=i+1;
-                break;
+        int n = s.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            vector<char> ans;
+
+            ans.push_back(s[i]);
+
+            for (int j = i + 1; j < n; j++) {
+
+                bool found = false;
+
+                for (int k = 0; k < ans.size(); k++) {
+                    if (s[j] == ans[k]) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found) {
+                    break;
+                }
+
+                ans.push_back(s[j]);
+
+                count = max(count, (int)ans.size());
             }
+
+            count = max(count, (int)ans.size());
         }
-        ans=max(ans,right-left+1);
-       }
-       return ans;
+
+        return count;
     }
 };
